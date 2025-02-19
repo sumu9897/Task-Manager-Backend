53a120592f30b0b8729935cc98595d8322e8f084
sumu9897 <sumon.bncd@gmail.com>
2025-02-19 21:59:53 +0600

    Database Connected

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const express = require("express")
-const app = express();
+const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/",(req,res) =>{
-    res.send("Task Management Backend")
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmec6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient instance
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(port,() => {
-    console.log(`Task Manager running ${port}`)
-})
+async function run() {
+  try {
+    await client.connect(); // Ensure connection to MongoDB
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected to MongoDB successfully!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Task Management Backend");
+});
+
+app.listen(port, () => {
+  console.log(`Task Manager running on port ${port}`);
+});
